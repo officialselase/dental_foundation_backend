c53d79b0f8c4db4bdc7c1384e2f39d87fcd0dd23
Sama <etornamasamoah@gmail.com>
2025-08-10 04:34:02 +0000

    s-init2

--- dental_foundation_backend/settings.py
+++ dental_foundation_backend/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')  # Use .env in production
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool) 
+#DEBUG = config('DEBUG', default=False, cast=bool) 
+DEBUG = True
 
 ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', default='*').split(',')
 
